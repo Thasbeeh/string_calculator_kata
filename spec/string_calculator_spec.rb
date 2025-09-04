cd622d92c7e96d8435abd3ec97caec422b5e81b9
Thasbeeh <ahmedthasbeeh@gmail.com>
2025-09-04 23:30:04 +0530

    test: add test for sum of n numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,5 +15,11 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1,2")).to eq(3)
       expect(StringCalculator.add("10,20")).to eq(30)
     end
+
+    it "returns the sum for n numbers" do
+      expect(StringCalculator.add("7,8,9")).to eq(24)
+      expect(StringCalculator.add("10,20,30,40")).to eq(100)
+      expect(StringCalculator.add("1,2,3,  4,5,6,7")).not_to eq(1)
+    end
   end
 end
