98d3fcb41f7b53f61afdc8a4cd81bcc88d132cc2
Thasbeeh <ahmedthasbeeh@gmail.com>
2025-09-05 02:31:10 +0530

    feature: handle delimiter in header

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,9 +2,14 @@
 
 # StringCalculator is a simple calculator that adds numbers provided in a string format.
 class StringCalculator
+  require 'pry'
   def self.add(input)
     if input.empty?
       0
+    elsif input.start_with?("//")
+      delimiter, numbers = input[2..].split("\n", 2)
+      delimiter = Regexp.escape(delimiter)
+      numbers.split(delimiter).map(&:to_i).sum
     elsif !input.include?(",")
       input.to_i
     else
