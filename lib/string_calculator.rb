c33087c718fce8ddafec4bb93b892307b7e238de
Thasbeeh <ahmedthasbeeh@gmail.com>
2025-09-05 02:55:40 +0530

    add rubocop fixes

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,14 +6,18 @@ class StringCalculator
   def self.add(input)
     if input.empty?
       0
-    elsif input.start_with?("//")
-      delimiter, numbers = input[2..].split("\n", 2)
-      delimiter = Regexp.escape(delimiter)
+    elsif input.start_with?('//')
+      delimiter, numbers = extract_delimiter(input)
       numbers.split(delimiter).map(&:to_i).sum
-    elsif !input.include?(",")
+    elsif !input.include?(',')
       input.to_i
     else
-      input.gsub("\n", ",").split(",").map(&:to_i).sum
+      input.gsub("\n", ',').split(',').map(&:to_i).sum
     end
+  end
+
+  def self.extract_delimiter(input)
+    delimiter, numbers = input[2..].split("\n", 2)
+    [Regexp.escape(delimiter), numbers]
   end
 end
